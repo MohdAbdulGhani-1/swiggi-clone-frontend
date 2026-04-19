1619ae1e0498e5562dc2913ad6b52b66045c5c35
Mohd Abdul Ghani <mohdabdulghani177@gmail.com>
2026-04-19 22:36:37 +0530

    fix

--- src/utils/useRestaurantMenu.jsx
+++ src/utils/useRestaurantMenu.jsx
@@ -11,7 +11,7 @@ const useRestaurantMenu = (resId) => {
     try {
       const data = await fetch(`${import.meta.env.VITE_useRestaurentMenuUrl}${resId}`);
       const json = await data.json();
-      // console.log("Fetched Menu Data: ", json); 
+      console.log("Fetched Menu Data: ", json); 
       setMenu(json);
     } catch (error) {
       console.log("Error while fetching the MENU_API:: ", error);
