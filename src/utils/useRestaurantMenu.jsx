488d62a040e4cbdcc41d6cf7acd2b4620fbe565c
Mohd Abdul Ghani <mohdabdulghani177@gmail.com>
2026-04-19 23:01:59 +0530

    update

--- src/utils/useRestaurantMenu.jsx
+++ src/utils/useRestaurantMenu.jsx
@@ -9,10 +9,16 @@ const useRestaurantMenu = (resId) => {
  
   const fetchData = async () => {
     try {
-      const data = await fetch(`${import.meta.env.VITE_useRestaurentMenuUrl}${resId}`);
+      const data = await fetch(`${import.meta.env.VITE_RestaurentMenuUrl}${resId}`);
+      // fetch(`${import.meta.env.VITE_useRestaurentMenuUrl}${resId}`);
       const json = await data.json();
       console.log("Fetched Menu Data: ", json); 
       setMenu(json);
+      const corsError = json?.corsfix_error ;
+      console.log(corsError);
+      if (corsError) {
+        console.log("CORS Error: ", corsError);
+      }
     } catch (error) {
       console.log("Error while fetching the MENU_API:: ", error);
     }
